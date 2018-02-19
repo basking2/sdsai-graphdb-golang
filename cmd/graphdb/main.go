57f8a6028c9070d3efd3e7db6664cf01d398b987
Sam Baskinger <basking2@yahoo.com>
2018-02-18 23:21:13 -0600

    Server.go: Add handler and prep for actual implementation.

--- cmd/graphdb/main.go
+++ cmd/graphdb/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
   "github.com/BurntSushi/toml"
-  "github.com/graymeta/stow"
-
-  // Pull in so all types of storage load.
-  "github.com/basking2/sdsai-graphdb-golang/pkg/sdsai/graphdb"
 )
 
 func main() {
   c := config{}
 
+  // Load defaults.
   toml.Decode(`
     [Storage]
     type = "local"
@@ -20,17 +17,5 @@ func main() {
   println("Got Type: ", c.Storage["type"])
   println("Got Path: ", c.Storage["path"])
 
-  location, err := stow.Dial(c.Storage["type"], c.Storage)
-  if err != nil {
-    panic(err.Error())
-  }
-
-  graphdb, err := graphdb.NewGraphDb("graphdb", location)
-  if err != nil {
-    panic(err.Error())
-  }
-
   serve(&c)
-
-  graphdb.Close()
 }
--- cmd/graphdb/server.go
+++ cmd/graphdb/server.go
@@ -2,11 +2,34 @@ package main
 
 import (
   "net/http"
+  "github.com/graymeta/stow"
+  "github.com/basking2/sdsai-graphdb-golang/pkg/sdsai/graphdb"
 )
 
 
 func serve(c *config) error {
+  location, err := stow.Dial(c.Storage["type"], c.Storage)
+  if err != nil {
+    panic(err.Error())
+  }
+
+  graphdb, err := graphdb.NewGraphDb("graphdb", location)
+  if err != nil {
+    panic(err.Error())
+  }
+
+  mux := http.NewServeMux()
+  mux.HandleFunc("/db/", func(rw http.ResponseWriter, req *http.Request){
+    // FIXME - write in database manipulation here.
+    rw.Write([]byte("Got it.\n"))
+  })
+
   s := http.Server{}
   s.Addr = "0.0.0.0:8080"
+  s.RegisterOnShutdown(func() {
+    graphdb.Close()
+  })
+  s.Handler = mux
+
   return s.ListenAndServe()
 }
